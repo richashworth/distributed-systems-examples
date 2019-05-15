c9682046b1a40f3e50dd32788d015f56fd84f3db
Richard Ashworth <richardashworth575@gmail.com>
2019-05-15 12:10:27 +0100

    run loop

--- src/main/scala/fds/scala/queue/Producer.scala
+++ src/main/scala/fds/scala/queue/Producer.scala
@@ -21,16 +21,13 @@ object Producer extends App {
   val con: Connection = ConnectionFactory.connect("127.0.0.1")
   val wc: WriteChannel = con.publish(ChannelName)
 
-  wc.write("Hello Consumer")
-  wc.write(EndOfStreamMarker)
-
   val fileName = "access.log"
   val logLines = scala.io.Source.fromFile(fileName).getLines
 
   // TODO use logLines to write each line onto the channel
-//  for (line <- logLines) {
-//    wc.write(line)
-//  }
+  for (line <- logLines) {
+    wc.write(line)
+  }
   // TODO write an end of stream marker
   wc.write(EndOfStreamMarker)
 
